8db217471d2ed1966e1b2ad5ca6e0dcaa4dfb8ce
haimanti <khepiburi@gmail.com>
2021-01-12 18:55:20 +0530

    added limits-service

---
+++ limits-service/src/main/java/com/haimanti/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,21 @@
+package com.haimanti.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haimanti.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration config;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(config.getMaximum(),config.getMinimum());
+	}
+	
+	
+}
